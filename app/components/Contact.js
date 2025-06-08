4843dea68edc9d7df08cd01cfe99aad5a0cf275a
ahuja2666 <ahujaharshit26@gmail.com>
2025-06-08 19:16:50 +0530

    add contact info

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,24 +1,41 @@
 import { Grid } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-const contacts = [{ img: "/Discord.png", alt: "disord icon", href: "https://discordapp.com/users/938844640004673536", text: "!harshit#7305" },
-{ img: "/Email.png", alt: "email icon", href: "mailto: ahujaharshit26@gamil.com", text: "ahujaharshit26@gamil.com" }
-
-]
-// { img: "/Linkedin.png", alt: "linkedin icon", href: "https://www.linkedin.com/in/ahujaharshit26/", text: "@ahujaharshit26" }
+const contacts = [
+  {
+    img: "/Discord.png",
+    alt: "disord icon",
+    href: "https://discordapp.com/users/938844640004673536",
+    text: "!harshit#7305",
+  },
+  {
+    img: "/Email.png",
+    alt: "email icon",
+    href: "mailto: ahujaharshit26@gamil.com",
+    text: "ahujaharshit26@gamil.com",
+  },
+  {
+    img: "/Linkedin.png",
+    alt: "linkedin icon",
+    href: "https://www.linkedin.com/in/ahujaharshit26/",
+    text: "@ahujaharshit26",
+  },
+];
 
 export default function Contact(props) {
-  const { contactPage } = props
+  const { contactPage } = props;
   return (
     <div className="ml-36 mr-32 mt-16 max-sm:ml-5 max-sm:mr-5">
       <div className="flex flex-col gap-10">
         <div className="flex justify-between items-center">
           <div className="flex gap-3 items-center">
             <div className="text-3xl font-medium">
-              <span className="text-[#C778DD]">{(contactPage) ? "/" : "#"}</span>
+              <span className="text-[#C778DD]">{contactPage ? "/" : "#"}</span>
               <span className="text-white">contact</span>
             </div>
-            {(!contactPage) && <div className=" w-40 h-[1px] bg-[#C778DD] max-md:hidden"></div>}
+            {!contactPage && (
+              <div className=" w-40 h-[1px] bg-[#C778DD] max-md:hidden"></div>
+            )}
           </div>
         </div>
         <div>
@@ -27,41 +44,46 @@ export default function Contact(props) {
               <div className="text-[#ABB2BF] font-normal text-base flex flex-col gap-7">
                 <p>
                   Feel free to get in touch with me for any inquiries, feedback,
-                  collaboration opportunities, or freelance work. I&apos;m always
-                  excited to connect and explore new possibilities in the world
-                  of web development and technology.
+                  collaboration opportunities, or freelance work. I&apos;m
+                  always excited to connect and explore new possibilities in the
+                  world of web development and technology.
                 </p>
                 <p>
                   Your feedback is invaluable to me, as it helps me refine my
-                  skills and deliver better solutions. If you&apos;re looking for a
-                  passionate and skilled full-stack web app developer to join
-                  your team or collaborate on a project, don&apos;t hesitate to reach
-                  out. Let&apos;s work together to create innovative and impactful
-                  digital experiences.
+                  skills and deliver better solutions. If you&apos;re looking
+                  for a passionate and skilled full-stack web app developer to
+                  join your team or collaborate on a project, don&apos;t
+                  hesitate to reach out. Let&apos;s work together to create
+                  innovative and impactful digital experiences.
                 </p>
               </div>
             </Grid>
             <Grid item xs={12} md={6} lg={6}>
               <div className="flex justify-end max-md:justify-start">
                 <div className="border border-[#ABB2BF] p-4 flex flex-col gap-3 max-sm:w-full">
-                  <p className="text-white font-semibold text-base">Message me here</p>
-                  {
-                    contacts?.map((item, idx) => {
-                      return (
-                        <div key={idx + item?.href} className="flex gap-1 items-center">
-                          <Image
-                            src={item?.img}
-                            height={32}
-                            width={32}
-                            alt={item?.alt}
-                          />
-                          <Link target="_blank" href={item?.href}><span className="text-[#ABB2BF] text-base font-normal">{item?.text}</span></Link>
-                        </div>
-                      )
-                    })
-                  }
-
-
+                  <p className="text-white font-semibold text-base">
+                    Message me here
+                  </p>
+                  {contacts?.map((item, idx) => {
+                    return (
+                      <div
+                        key={idx + item?.href}
+                        className="flex gap-1 items-center"
+                      >
+                        <Image
+                          src={item?.img}
+                          height={32}
+                          width={32}
+                          alt={item?.alt}
+                        />
+                        <Link target="_blank" href={item?.href}>
+                          <span className="text-[#ABB2BF] text-base font-normal">
+                            {item?.text}
+                          </span>
+                        </Link>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             </Grid>
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,27 +1,32 @@
 import { Grid } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import IntroText from './IntroText'
-import AnimatedButton from "./AnimatedButton"
+import IntroText from "./IntroText";
+import AnimatedButton from "./AnimatedButton";
 
 export default function Main() {
   return (
-    <main className='text-3xl text-white ml-36 mr-32 py-2  max-sm:mx-5  max-sm:mt-3 flex flex-col gap-10 justify-between'>
+    <main className="text-3xl text-white ml-36 mr-32 py-2  max-sm:mx-5  max-sm:mt-3 flex flex-col gap-10 justify-between">
       <Grid container spacing={6} justifyContent="center" alignItems="center">
         <Grid item xs={12} sm={12} md={6} lg={6}>
-          <div className='flex flex-col gap-8'>
+          <div className="flex flex-col gap-8">
             <div>
-              <h2 className=' text-3xl font-semibold'>
-                Harshit is a <span className='text-[#C778DD]'>Full Stack Web App Developer</span>
+              <h2 className=" text-3xl font-semibold">
+                Harshit is a{" "}
+                <span className="text-[#C778DD]">
+                  Full Stack Web App Developer
+                </span>
               </h2>
             </div>
             <div>
-              <p className='text-[#ABB2BF] font-normal text-sm'>
-                From Frontend Flourishes to Backend Brilliance: Powering Digital Dreams with Full Stack Wizardry</p>
+              <p className="text-[#ABB2BF] font-normal text-sm">
+                From Frontend Flourishes to Backend Brilliance: Powering Digital
+                Dreams with Full Stack Wizardry
+              </p>
             </div>
             <div>
               <Link href={"/contact"}>
-                <AnimatedButton btnText={'Contact Me!!'} />
+                <AnimatedButton btnText={"Contact Me!!"} />
               </Link>
             </div>
           </div>
@@ -43,42 +48,30 @@ export default function Main() {
                 alt="box"
                 className=" inline mr-2 mb-0.5"
               />
+              <span className="text-[#ABB2BF]">Currently working at </span>
+              <span>Solvative Private Limited</span>
             </div>
           </div>
-
-
         </Grid>
       </Grid>
       <div className="p-0 m-0 box-border w-[85%] mx-auto max-md:hidden">
         <fieldset className="border border-[#ABB2BF] pl-[2%] border-b-0">
           <div className=" text-2xl text-center p-3 pt-1 font-normal">
-            <IntroText words={'Turning coffee into code, one cup at a time!'} />
+            <IntroText words={"Turning coffee into code, one cup at a time!"} />
           </div>
           <legend>
-            <Image
-              height={25}
-              width={25}
-              src={"/comma.png"}
-              alt="comma"
-            />
+            <Image height={25} width={25} src={"/comma.png"} alt="comma" />
           </legend>
         </fieldset>
         <fieldset className="border-t border-[#ABB2BF] pl-[95%] -mt-2 max-md:pl-[88%]">
           <legend>
-            <Image
-              height={25}
-              width={25}
-              src={"/comma.png"}
-              alt="comma"
-            />
+            <Image height={25} width={25} src={"/comma.png"} alt="comma" />
           </legend>
         </fieldset>
         <div className="border border-[#ABB2BF] border-t-0 -mt-2 ml-[70%]">
           <p className=" text-2xl text-center p-3 font-normal">- Programmer</p>
         </div>
-
       </div>
-
     </main>
-  )
+  );
 }
